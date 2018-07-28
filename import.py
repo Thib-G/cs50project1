c0464b01345b21e7a47957821836937b99dd0955
Thib <thibaut.g@protonmail.com>
2018-07-28 16:36:41 +0200

    Add users and reviews, including pgcrypt extension to store passwords.

--- import.py
+++ import.py
@@ -12,8 +12,11 @@ def main():
     db.execute("DROP TABLE IF EXISTS books_tmp;")
     db.execute("DROP TABLE IF EXISTS books;")
     db.execute("DROP TABLE IF EXISTS authors;")
+    db.execute("DROP TABLE IF EXISTS users;")
+    db.execute("DROP TABLE IF EXISTS reviews;")
+    db.execute("CREATE EXTENSION IF NOT EXISTS pgcrypto;")
     db.execute("""
-        CREATE TABLE books_tmp (
+        CREATE TEMP TABLE books_tmp (
             id serial PRIMARY KEY,
             isbn text NOT NULL UNIQUE CHECK (char_length(isbn) = 10),
             title text NOT NULL,
@@ -33,24 +36,53 @@ def main():
             title text NOT NULL,
             author_id int REFERENCES authors(id),
             year_pub int NOT NULL
-        )
+        );
     """)
+    db.execute("""
+        CREATE TABLE users (
+            id uuid NOT NULL DEFAULT gen_random_uuid() PRIMARY KEY,
+            email text NOT NULL UNIQUE,
+            pwhash text NOT NULL
+        );
+    """)
+    db.execute("""
+        CREATE TABLE reviews (
+            id serial PRIMARY KEY,
+            user_id uuid REFERENCES users(id),
+            book_id int REFERENCES books(id),
+            rating int NOT NULL CHECK (rating >= 1 AND rating <= 5),
+            review text NOT NULL
+        );
+    """)
+    users = [
+        { "email": "thib@example.com", "password": "password123" },
+        { "email": "pom@example.com", "password": "meuhmeuh" }
+    ]
+    for user in users:
+        db.execute("""
+            INSERT INTO users (email, pwhash)
+                VALUES (:email, crypt(:password, gen_salt('bf', 8)));
+        """, { "email": user.get("email"), "password": user.get("password") })
+
     with open("books.csv", "r") as f:
         reader = csv.reader(f)
         # skip header
         next(reader)
 
         for isbn, title, author, year in reader:
-            db.execute("INSERT INTO books_tmp (isbn, title, author, year_pub) VALUES (:isbn, :title, :author, :year_pub)",
-                        {"isbn": isbn, "title": title, "author": author, "year_pub": year})
+            db.execute("""
+                INSERT INTO books_tmp (isbn, title, author, year_pub)
+                    VALUES (:isbn, :title, :author, :year_pub);
+                """,
+                {"isbn": isbn, "title": title, "author": author, "year_pub": year})
         
-        db.execute("INSERT INTO authors (name) SELECT DISTINCT author FROM books_tmp;")
-        db.execute("""
-            INSERT INTO books (isbn, title, author_id, year_pub)
-                SELECT b.isbn, b.title, a.id author_id, b.year_pub
-                FROM books_tmp b JOIN authors a ON b.author = a.name;  
-        """)
-        db.execute("DROP TABLE books_tmp;")
+    db.execute("INSERT INTO authors (name) SELECT DISTINCT author FROM books_tmp;")
+    db.execute("""
+        INSERT INTO books (isbn, title, author_id, year_pub)
+            SELECT b.isbn, b.title, a.id author_id, b.year_pub
+            FROM books_tmp b JOIN authors a ON b.author = a.name
+            ORDER BY b.id;  
+    """)
 
     db.commit()
 
